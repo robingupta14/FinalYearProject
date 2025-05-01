43f3c89b1bdef2f1890c7302c5c708d7917bacfe
Robin <robin1gupta.2003@gmail.com>
2025-05-01 17:59:14 +0100

    start..

---
+++ preprocessor/C_Preprocessor.py
@@ -0,0 +1,10 @@
+# Extraneous whitespace, comments, formatting, documentation, and typically print
+# statements and exception strings are all semantically irrelevant.
+
+# Renaming 
+
+# Expansion is the process of replacing referential definitions with the actual value.
+
+# Constant folding
+
+# Loop canonicalization
