df5dafd5d13a38947771d8d0344b9ece93a01cfd
Robin <robin1gupta.2003@gmail.com>
2025-05-05 15:24:53 +0100

    C preproc

--- preprocessor/C_Preprocessor.py
+++ preprocessor/C_Preprocessor.py
@@ -1,8 +1,3 @@
-import subprocess
-import os
-import re
-import string
-from tree_sitter import Language, Parser
 # Extraneous whitespace, formatting -> Not stored in tree sitter ASTs
     # comments, documentation, -> delete comment nodes; c doesn't have docstrings.
     # Can't safely remove prints and exception strings in C because they can cause segfaults -> that is a CWE.
@@ -18,9 +13,29 @@ from tree_sitter import Language, Parser
 
 # Loop canonicalization - if the software exists for it already.
 
-# Tools That Help
 # Task	Tool / Approach
 # AST parsing	Tree-sitter
 # Macro expansion	clang -E for C/C++, or Roslyn for C#
 # Identifier renaming	Tree-sitter AST + symbol table
 # Constant folding	Custom AST evaluation
+
+import subprocess
+import os
+import re
+import string
+from tree_sitter import Language, Parser
+
+C_LANGUAGE = Language('parsers/c-parsers.so', 'c')
+
+parser = Parser()
+parser.set_language(C_LANGUAGE)
+
+source_code = """
+int main() {
+    int a = 1 + 2;
+    return a;
+}
+"""
+
+tree = parser.parse(bytes(source_code, "utf8"))
+print(tree.root_node)
