46291e46d2d08260fe2f585653e4eee2ff1343aa
Robin <robin1gupta.2003@gmail.com>
2025-05-11 18:58:26 +0100

    Start html. seems difficult as it needs CPP and js too

--- preprocessor/HTML_Preprocessor.py
+++ preprocessor/HTML_Preprocessor.py
@@ -1,1 +1,11 @@
-ughhhh pain :/
+# GOAL: Carry out AST Generation using the generated Parser from Tree-sitter:
+from tree_sitter_languages import get_language, get_parser
+import tempfile
+import subprocess
+import re
+import os
+from collections import defaultdict
+
+# 1) Extraneous whitespace, formatting -> Removed by this step as it's not stored in Tree-sitter ASTs.
+html = get_language('html')
+parser = get_parser('html')
